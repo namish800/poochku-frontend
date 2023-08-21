3ee6a5b6bd219d37b37f9257bfd1f002fb8decd9
Namish pruthi <namishpruthi800@gmail.com>
2023-08-21 23:58:00 +0530

    Added EnquiryApi service

---
+++ src/services/EnquiryApi.js
@@ -0,0 +1,54 @@
+// services/enquiryApi.js
+import axios from 'axios';
+import API_CONFIG from '../config/apiConfig';
+
+
+const instance = axios.create({
+  baseURL: API_CONFIG.baseUrl,
+});
+
+const enquiryApi = {
+  saveBestPriceEnquiry: async (userId, petId) => {
+    try {
+      const response = await instance.post('/enquiry/best-price', null, {
+        params: {
+          userId,
+          petId,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+  
+  saveSeeMoreEnquiry: async (userId, petId) => {
+    try {
+      const response = await instance.post('/enquiry/see-more', null, {
+        params: {
+          userId,
+          petId,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+  
+  saveWhatsappEnquiry: async (userId, petId) => {
+    try {
+      const response = await instance.post('/enquiry/whatsapp-inquiry', null, {
+        params: {
+          userId,
+          petId,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+};
+
+export default enquiryApi;
