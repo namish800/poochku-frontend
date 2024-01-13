361d72d4f45c1c0a550681f6cb19b984aa945788
namish800 <namishpruthi800@gmail.com>
2024-01-13 17:15:35 +0530

    updates workflow file

---
+++ src/Components/Pages/Products/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Product = ({ product }) => {
+  const { id, name, description, price, imageUrl, inStock } = product;
+
+  return (
+    <div className={`product ${inStock ? '' : 'out-of-stock'}`}>
+      <img src={imageUrl} alt={name} />
+      <h3>{name}</h3>
+      <p className='productdetail'>{description}</p>
+      <p id='productdetail'>&#x20b9; {price}</p>
+      {inStock ? (
+        <button>Add to Cart</button>
+      ) : (
+        <p className="out-of-stock-message">Out of Stock</p>
+      )}
+    </div>
+  );
+};
+
+export default Product;
