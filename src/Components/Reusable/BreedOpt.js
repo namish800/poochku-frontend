1cb3e5500fa34a83c337339489d4c0d0acc676e0
namish800 <namishpruthi800@gmail.com>
2024-01-26 03:56:38 +0530

    fixes breed list

--- src/Components/Reusable/BreedOpt.js
+++ src/Components/Reusable/BreedOpt.js
@@ -6,7 +6,7 @@ const BreedOpt = ({ selectedBreed, setSelectedBreed }) => {
   return (
     <select className='filterInput' value={selectedBreed}
 	onChange={(e) => setSelectedBreed(e.target.value)}>
-        <option value="Breed">Breed</option>
+        <option value="">Breed</option>
 		{
 			dogDb.map((e) => <option key={e.id} value={e.breed}>{e.breed}</option>)
 		}
