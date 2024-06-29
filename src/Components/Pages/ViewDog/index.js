3616f3585950259c212c5c348b447f2cac53ac2c
Satyam <satyam.mahajan98@gmail.com>
2024-06-30 01:13:08 +0530

    Styled Dog Card

--- src/Components/Pages/ViewDog/index.js
+++ src/Components/Pages/ViewDog/index.js
@@ -10,6 +10,20 @@ import dogDB from '../../Reusable/breeds';
 import { Rating, Skeleton } from '@mui/material';
 import EnquiryModal from '../../Reusable/EnquiryModal/EnquiryModal';
 import MobileNav from '../../Reusable/MobileNav'
+import UserPic from '../../../Assets/userPic.png'
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import FemaleIcon from '@mui/icons-material/Female';
+import MaleIcon from '@mui/icons-material/Male';
+import CakeIcon from '@mui/icons-material/Cake';
+import VaccinesIcon from '@mui/icons-material/Vaccines';
+import KidsFreind from '../../../Assets/kidfriendly.png';
+import OtherPet from '../../../Assets/otherpet.png';
+import Shedding from '../../../Assets/shedding.png';
+import GroomingDog from '../../../Assets/grooming.png';
+import EnergeticDog from '../../../Assets/energylevels.png';
+import IntelligentDog from '../../../Assets/intelligence.png';
+import TrainedDog from '../../../Assets/trainability.png';
+
 
 const ViewDog = () => {
   const params = useParams();
@@ -76,81 +90,100 @@ const ViewDog = () => {
           <div className='pupListWrapper'>
             <div className='pageHeadingSticky viewDog'>
               <div>
-                <h1 className='buyPageHeading'>Adopt { details?.name ? details?.name : `Pooch`}</h1>
+                <h1 className='buyPageHeading'>Adoption Center </h1>
+                {/* <h1 className='buyPageHeading'>Adopt { details?.name ? details?.name : `Pooch`}</h1> */}
                 {/* <p className='buyPageInfo'>{`Browse  >   View Dog`}</p> */}
               </div>
-              <div className='actionWrapper'>
+              {/* <div className='actionWrapper'>
                 <button className='whatsappEnquire' onClick={waEnquire}><img src={whatsappIcon} />Enquire</button>
                 {details && details?.service?.serviceCode !== "A" && <button className='bestBuy' onClick={enquiryRequest}>Get Best Price</button>}
-              </div>
+              </div> */}
             </div>
             {details && info ?
               (<div className='dogViewWrapper'>
+                <div className='galleryWrapper'>
+                  <h2 className='font-face-D'>{ details?.name ? details?.name : `Adopt Pooch`}</h2>
+                  <p>{details?.description ? details?.description : "No Description is available"}</p>
+                  <img className='dogPic' src={details?.imageUrls ? details?.imageUrls?.[0] : dog}/>
+                </div>
                 <div className='mainInfo'>
+                  <div className='listerDetailWrapper'>
+                    <img src={details?.owner?.image ? details?.owner?.image : UserPic} alt="dog listed for adoption by"/>
+                    <p>{`${details?.owner?.fName} ${details?.owner?.lName}`}</p>
+                    <button className='whatsappEnquire' onClick={waEnquire}><img src={whatsappIcon} />Enquire</button>
+                  </div>
+                    
                   <div className='directInfoWrapper'>
                     <div className='directInfo'>
+                      <LocationOnIcon className='dogDetailIcon' />
                       <p className='InfoHeader'>Location</p>
                       <p>{details?.location ? details?.location : "N/A"}</p>
                     </div>
                     <div className='directInfo'>
+                      <div>
+                        <FemaleIcon className='dogDetailIcon'/>
+                        <MaleIcon className='dogDetailIcon'/>
+                      </div>
                       <p className='InfoHeader'>Gender</p>
                       <p>{details?.gender ? details?.gender : "N/A"}</p>
                     </div>
-                    <div className='directInfo'>
+                    {/* <div className='directInfo'>
                       <p className='InfoHeader'>Quality</p>
                       <p>{details?.quality ? details?.quality : "N/A"}</p>
-                    </div>
+                    </div> */}
                     <div className='directInfo'>
+                      <CakeIcon  className='dogDetailIcon'/>
                       <p className='InfoHeader'>Age </p>
-                      <p>{details?.age ? `${details?.age} days` : "N/A"}</p>
+                      <p>{`${details?.age?.years && details?.age?.years + " years"} ${details?.age?.months && "and " + details?.age?.months + " months"} old` }</p>
                     </div>
                     <div className='directInfo'>
+                      <VaccinesIcon  className='dogDetailIcon'/>
                       <p className='InfoHeader'>Vaccination Status </p>
                       <p>{details?.vaccination_status ? details.vaccination_status : "N/A"}</p>
                     </div>
-                    <div className='directInfo'>
+                    {/* <div className='directInfo'>
                       <p className='InfoHeader'>Listed By</p>
                       <p>{details?.fName ? details.fName : "N/A"}</p>
-                    </div>
+                    </div> */}
                   </div>
-                  <div className='descriptionWrapper'>
-                    <h4>{details?.name ? `About ${details?.name}` : `Description`}</h4>
-                    <p>{details?.description ? details?.description : "No Description is available"}</p>
-                    {/* <p>{info?.generalDescription ? info?.generalDescription : "No Description is available"}</p> */}
-                  </div>
-                  <div className='directInfoWrapper'>
+
+                  <div className='directInfoWrapper scores'>
                     <div className='directInfo'>
-                      <p className='InfoHeader'>Kids Friendly Score</p>
+                      <img src={KidsFreind} />
+                      <p className='InfoHeader'>Kids Friendliness</p>
                       <Rating value={info?.kidFriendlyScore} readOnly />
                     </div>
                     <div className='directInfo'>
-                      <p className='InfoHeader'>Other Pet Friendliness Score</p>
+                      <img src={OtherPet} />
+                      <p className='InfoHeader'>Other Pet Friendliness</p>
                       <Rating value={info?.dogFriendly} readOnly />
                     </div>
                     <div className='directInfo'>
+                      <img src={Shedding} />
                       <p className='InfoHeader'>Shedding</p>
                       <p><span>{info?.shedding}</span></p>
                     </div>
                     <div className='directInfo'>
-                      <p className='InfoHeader'>Ease in Grooming Score</p>
+                      <img src={GroomingDog} />
+                      <p className='InfoHeader'>Ease in Grooming</p>
                       <Rating value={info?.easyToGroom} readOnly />
                     </div>
                     <div className='directInfo'>
+                      <img src={EnergeticDog} />
                       <p className='InfoHeader'>Energy Levels</p>
                       <p><span>{info?.energyLevel}</span></p>
                     </div>
                     <div className='directInfo'>
+                      <img src={IntelligentDog} />
                       <p className='InfoHeader'>Intelligence</p>
                       <Rating value={info?.intelligence} readOnly />
                     </div>
                     <div className='directInfo'>
+                      <img src={TrainedDog} />
                       <p className='InfoHeader'>Trainability</p>
                       <Rating value={info?.easyToTrain} readOnly />
                     </div>
                   </div>
-                </div>
-                <div className='galleryWrapper'>
-                  <img className='dogPic' src={details?.imageUrls ? details?.imageUrls?.[0] : dog}/>
                 </div>
               </div>) 
             : 
